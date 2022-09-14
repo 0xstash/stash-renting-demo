2594708a3834407dbdc8bdb7aeed197513ab25d9
vivek <appusvivek1@gmail.com>
2022-09-14 17:32:40 +0530

    Signer added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-
+import { useSigner } from 'wagmi'
 
 export default function Home() {
+
+  const { data: signer } = useSigner();
+
   return (
    <div>
     <p>Here</p>
-    <ConnectButton />
+      <ConnectButton />
+       {signer?._address && <p>Signer available for : {signer._address}</p>}
    </div>
   )
 }
