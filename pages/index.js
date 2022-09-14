1975a0540ecb79f02ff0ae312d57e230db7c01a5
vivek <appusvivek1@gmail.com>
2022-09-14 18:58:57 +0530

    Error while importing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useSigner } from 'wagmi'
-
+import { Stash } from 'stash-renting-sdk'
+import { useEffect } from 'react';
 export default function Home() {
 
   const { data: signer } = useSigner();
+
+  useEffect(() => {
+    if(signer?._address) {
+      // const stash = new Stash();
+      // console.log('stash', stash);
+    }
+  }, [signer?._address])
 
   return (
    <div>
