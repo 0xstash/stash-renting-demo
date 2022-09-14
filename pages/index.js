79d98387ea3143fe7262afe3a92be38ec5c6d218
vivek <appusvivek1@gmail.com>
2022-09-14 16:13:09 +0530

    added raibow kit for wallet connection

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 
 export default function Home() {
   return (
-   <div>Hello</div>
+   <div>
+    <p>Here</p>
+    <ConnectButton />
+   </div>
   )
 }
