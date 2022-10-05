856bfedfe79d80239b2ec1314a72537e95ffea7d
Bertan Ege Teberci <bertanteberci@gmail.com>
2022-10-05 15:29:04 +0200

    update sdk call params and arrange fields

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useSigner } from 'wagmi'
-import { Stash, NFTStandard, Chain, WrapperFactory, PaymentToken } from 'stash-renting-sdk'
+import { Stash, NFTStandard, Chain } from 'stash-renting-sdk'
 import { useState } from 'react';
 import { Box, Button, Flex, Heading, Text, Input, FormControl, FormLabel, Switch } from '@chakra-ui/react';
 
@@ -9,8 +9,8 @@ export default function Home() {
   const [nftData, setNftData] = useState();
   const [nftAddress, setNftAddress] = useState();
 
+  const [apiKey, setApiKey] = useState('056a11df8ec19003cd4dd4b34e05c55c55c1e06d');
   const [tokenVal, setToken] = useState();
-  const [rentalId, setRentalId] = useState();
   const [lendBtn, setLendBtn] = useState(false);
   const [rentBtn, setRentBtn] = useState(false);
   const [apiBtn, setApiBtn] = useState(false);
@@ -20,20 +20,17 @@ export default function Home() {
   const [erc20Address, setErc20Address] = useState('0xd73D2595A37AC493f8c4c727b4161995F09eEb13');
   const [revShare, setRevShare] = useState();
   const [buyPrice, setBuyPrice] = useState();
-  const [wrap, setWrap] = useState(true);
-  const [rentDuration, setRentDuration] = useState(1);
+  const [maxRentalDays, setMaxRentalDays] = useState();
+  const [rentDuration, setRentDuration] = useState();
 
   const { data: signer } = useSigner();
-  const apiKey = '27afdad77ae112cffa47de5c3236a63da959b891';
 
   const handleGetNFTData = async () => {
-    if(signer?._address) {
+    if(signer?._address && nftAddress && tokenVal) {
       setApiBtn(true);
       const stash = new Stash(apiKey, signer, Chain.ETH, '');
       const stashAPI =  stash.contracts.api;
-      const contractAddress = '0x001';
-      const token = '123456';
-      stashAPI.getNFTData(contractAddress, token).then((res) => {
+      stashAPI.getAsset(nftAddress, parseInt(tokenVal)).then((res) => {
         if(res.data) {
           setNftData(res.data)
           setApiBtn(false);
@@ -43,371 +40,53 @@ export default function Home() {
         setApiBtn(false);
       });
     }
-  }; 
+  };
 
-  const erc721Abi = [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "approved",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "Approval",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "operator",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "bool",
-          "name": "approved",
-          "type": "bool"
-        }
-      ],
-      "name": "ApprovalForAll",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "approve",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "mint",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "safeTransferFrom",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bytes",
-          "name": "data",
-          "type": "bytes"
-        }
-      ],
-      "name": "safeTransferFrom",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "operator",
-          "type": "address"
-        },
-        {
-          "internalType": "bool",
-          "name": "approved",
-          "type": "bool"
-        }
-      ],
-      "name": "setApprovalForAll",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "Transfer",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "transferFrom",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        }
-      ],
-      "name": "balanceOf",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "getApproved",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "operator",
-          "type": "address"
-        }
-      ],
-      "name": "isApprovedForAll",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "name",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "ownerOf",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "bytes4",
-          "name": "interfaceId",
-          "type": "bytes4"
-        }
-      ],
-      "name": "supportsInterface",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "symbol",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "tokenURI",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
-
-  const handleNFTLend = () => {
+  const handleNFTLend = async () => {
     if(signer?._address) {
-      if(nftAddress && tokenVal && expiry && perDayPrice && erc20Address && revShare && buyPrice) {
+      if(nftAddress && tokenVal && expiry && perDayPrice && erc20Address && revShare && buyPrice && maxRentalDays) {
         setLendBtn(true);
         const stash = new Stash(apiKey, signer, Chain.GOERLI, { 
-          ERC721ContractAddress: nftAddress,
-          //'0xF6a106Dc24176A31fBc75daAa4166745Fa8cbc2b',
-          ERC721ContractABI: erc721Abi
+          ERC721ContractAddress: nftAddress
         } );
         const stashMarket = stash.contracts.market;
+        
+        // Uncomment for testing wrapping
+        // var wrappedAddress = null;
+        // try {
+          // const wrapTxn = await stash.contracts.wrapperFactory.wrap(
+          //     1, 
+          //     nftStandard, 
+          //     (res) => {
+          //         console.log('result:', res);
+          //         wrappedAddress = res.args.wrappedAddress;
+          //     },
+          //     (error) => {
+          //         if (error) {
+          //             console.log('error', error.message);
+          //         }
+          //     });
+          // await wrapTxn?.wait();
+        // } catch {
+        //   console.log('wrap failed');
+        // }
+        // console.log('wrapped address', wrappedAddress);
 
-        stashMarket.lend(
+        // Uncomment for testing setRentalTerms call alone
+        // const txn = await stashMarket.contract.setRentalTerms(
+        //   "0x52dFC461c81a7914D3C978047ac463c57086Bb61",
+        //   1,
+        //   1,
+        //   60 * 60 * 24,
+        //   ethers.utils.parseEther("0.05"),
+        //   100,
+        //   10.0,
+        //   erc20Address,
+        //   100
+        // );
+        // print('txn', txn);
+
+        await stashMarket.lend(
           parseInt(tokenVal),
           nftStandard,
           parseInt(expiry),
@@ -415,12 +94,12 @@ export default function Home() {
           erc20Address,
           parseFloat(revShare),
           parseFloat(buyPrice),
-          wrap,
+          parseInt(maxRentalDays),
           (success) => {
             // On success
             if(success.args.rentalId) {
               // rentalId in bignumber success.args.rentalId
-              setRentalId(success.args.rentalId.toHexString());
+              console.log('rental Id', success.args.rentalId.toHexString());
               //Transaction has success.transactions[0].txn_hash
               setLendBtn(false);
             }
@@ -433,22 +112,21 @@ export default function Home() {
           }
         );
       }
-      
     }
   }
 
-  const handleNFTRent = () => {
+  const handleNFTRent = async () => {
     if(signer?._address) {
-      if(rentalId && rentDuration) {
+      if(nftAddress && tokenVal && rentDuration) {
         setRentBtn(true);
         const stash = new Stash(apiKey, signer, Chain.GOERLI, { 
-          ERC721ContractAddress: nftAddress,
-          ERC721ContractABI: erc721Abi
+          ERC721ContractAddress: nftAddress
         } );
         const stashMarket = stash.contracts.market;
   
-        stashMarket.rent(
-          parseInt(rentalId),
+        await stashMarket.rent(
+          parseInt(tokenVal),
+          nftStandard,
           parseInt(rentDuration),
           (success) => {
             // success.success will give the status 
@@ -460,8 +138,8 @@ export default function Home() {
             // On error
             console.log('error triggered',error);
             setRentBtn(false);
-          } 
-        )
+          }
+        );
       }
     }
   }
@@ -474,21 +152,9 @@ export default function Home() {
         <>
           <Flex flexDirection={'column'} mt={10} gap={10}>
             <Flex flexDirection={'column'} gap={5}>
-              <Heading size={'md'}>Get NFT Data</Heading>
-              <Flex gap={10}>
-                <Button w={'fit-content'} onClick={handleGetNFTData} isLoading={apiBtn}>Get NFT Data</Button>
-                  {nftData &&
-                  <Box p={4}  borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    <Text>Token ID: {nftData?.token_id}</Text>
-                    <Text>NFT Address: {nftData?.nft_address}</Text>
-                  </Box>
-                  }
-              </Flex>
-            </Flex>
-            <Flex flexDirection={'column'} gap={5}>
-              <Heading size={'md'}>Lend Asset</Heading>
-              <Input placeholder='NFT Contract Address' size='md' width={'25%'} value={nftAddress} onChange={(e) => setNftAddress(e.target.value)}/>
-              <Input placeholder='Token ID' size='md' width={'25%'} value={tokenVal} onChange={(e) => setToken(e.target.value)}/>
+              <Heading size={'md'}>NFT Data</Heading>
+              <Input placeholder='NFT Contract Address' size='md' width={'35%'} value={nftAddress} onChange={(e) => setNftAddress(e.target.value)}/>
+              <Input placeholder='Token ID' size='md' width={'35%'} value={tokenVal} onChange={(e) => setToken(e.target.value)}/>
               <FormControl display='flex' alignItems='center'>
                 <FormLabel htmlFor='nftStandard' mb='0'>
                   E721 / E1155
@@ -501,30 +167,33 @@ export default function Home() {
                   }
                 }} />
               </FormControl>
-              <Input type={'number'} placeholder='Expiry in days' size='md' width={'25%'} value={expiry} onChange={(e) => setExpiry(e.target.value)}/>
-              <Input type={'number'} placeholder='Per day price' size='md' width={'25%'} value={perDayPrice} onChange={(e) => setPerDayPrice(e.target.value)}/>
-              <Input placeholder='ERC20 Address' size='md' width={'25%'} value={erc20Address} onChange={(e) => setErc20Address(e.target.value)}/>
-              <Input type={'number'} placeholder='Revenue share' size='md' width={'25%'} value={revShare} onChange={(e) => setRevShare(e.target.value)}/>
-              <Input type={'number'} placeholder='Buy price' size='md' width={'25%'} value={buyPrice} onChange={(e) => setBuyPrice(e.target.value)}/>
-              <FormControl display='flex' alignItems='center'>
-                <FormLabel htmlFor='wrap' mb='0'>
-                  Wrap needed ?
-                </FormLabel>
-                <Switch id='wrap' defaultChecked={true} onChange={(e) => {
-                  if(e.target.checked) {
-                    setWrap(true);
-                  } else {
-                    setWrap(false);
+            </Flex>
+            <Flex flexDirection={'column'} gap={5}>
+              <Heading size={'md'}>Fetch Asset Information From API</Heading>
+              <Input placeholder='API Key' size='md' width={'35%'} value={apiKey} onChange={(e) => setApiKey(e.target.value)}/>
+              <Flex gap={10}>
+                <Button w={'fit-content'} onClick={handleGetNFTData} isLoading={apiBtn}>Fetch Info</Button>
+                  {nftData &&
+                  <Box p={4}  borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                    <Text>Token ID: {nftData?.token_id}</Text>
+                    <Text>NFT Address: {nftData?.nft_address}</Text>
+                  </Box>
                   }
-                }} />
-              </FormControl>
+              </Flex>
+            </Flex>
+            <Flex flexDirection={'column'} gap={5}>
+              <Heading size={'md'}>Lend Asset</Heading>
+              <Input type={'number'} placeholder='Expiry in days' size='md' width={'35%'} value={expiry} onChange={(e) => setExpiry(e.target.value)}/>
+              <Input type={'number'} placeholder='Per day price' size='md' width={'35%'} value={perDayPrice} onChange={(e) => setPerDayPrice(e.target.value)}/>
+              <Input placeholder='ERC20 Address' size='md' width={'35%'} value={erc20Address} onChange={(e) => setErc20Address(e.target.value)}/>
+              <Input type={'number'} placeholder='Revenue share' size='md' width={'35%'} value={revShare} onChange={(e) => setRevShare(e.target.value)}/>
+              <Input type={'number'} placeholder='Buy price' size='md' width={'35%'} value={buyPrice} onChange={(e) => setBuyPrice(e.target.value)}/>
+              <Input type={'number'} placeholder='Max rental days' size='md' width={'35%'} value={maxRentalDays} onChange={(e) => setMaxRentalDays(e.target.value)}/>
               <Button w={'fit-content'} onClick={handleNFTLend} isLoading={lendBtn}>Lend Asset</Button>
             </Flex>
             <Flex flexDirection={'column'} gap={5}>
               <Heading size={'md'}>Rent Asset</Heading>
-              <Input placeholder='Rental Id' size='md' width={'25%'} value={rentalId} onChange={(e) => setRentalId(e.target.value)}/>
-              <Input type={'number'} placeholder='Rent Duration' size='md' width={'25%'} value={rentDuration} onChange={(e) => setRentDuration(e.target.value)}/>
-
+              <Input type={'number'} placeholder='Rent Duration' size='md' width={'35%'} value={rentDuration} onChange={(e) => setRentDuration(e.target.value)}/>
               <Button w={'fit-content'} onClick={handleNFTRent} isLoading={rentBtn}>Rent Asset</Button>
             </Flex>
           </Flex>
